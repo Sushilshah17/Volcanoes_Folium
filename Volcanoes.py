404703c19ab50ecb9f240c7699a02542546b3d4c
Sushil Shah <sshah483@gatech.edu>
2020-12-15 14:40:43 -0800

    Added Layer Controls

--- Volcanoes.py
+++ Volcanoes.py
@@ -25,18 +25,21 @@ Height: %s m
 
 map = folium.Map(location=[46.776959526407474, -118.1774030427453], zoom_start = 5)
 
-fg = folium.FeatureGroup(name="My Map")
-
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el, na in zip(lat, lon, elevation, name):
     iframe = folium.IFrame(html=html %(na, na, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=(lt,ln), popup=folium.Popup(iframe), icon=folium.Icon(color=color_generator(el))))
+    fgv.add_child(folium.Marker(location=(lt,ln), popup=folium.Popup(iframe), icon=folium.Icon(color=color_generator(el))))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 1000000
 else 'yellow' if 1000000 <= x['properties']['POP2005'] < 2000000
 else 'black' if 2000000 <= x['properties']['POP2005'] < 50000000 else 'red'}))
 
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Volcanoes.html")
